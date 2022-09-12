a924c1bb3887766346be58837f3f7e9d10394150
almuly <Alexander.Mulyarchik@solbeg.com>
2022-09-12 19:56:45 +0300

    add: redux

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('/login')
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto);
--- app/next.config.js
+++ app/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withFonts = require('next-fonts');
 
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 module.exports = withFonts({
+  ...nextConfig,
   enableSvg: true,
   webpack(config, options) {
     return config;
   },
 });
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
